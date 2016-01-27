2cb1e6679734b606f5bc5b5cacf9dd99c791f81b
onglo <wertyedouard@gmail.com>
2016-01-27 15:49:01 +0000

    Main calculator, buttons working except for operators

--- calculator test/ViewController.swift
+++ calculator test/ViewController.swift
@@ -9,6 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // declare a string that the numbers will be stored in
+    var theNumber: String = ""
+    
+    // Function that is called when any of the buttons are pressed (will be using tags to identify which button it was)
+    @IBAction func buttonPressed(sender: AnyObject) {
+        // for testing just print the button tag
+        print(sender.tag)
+        // check if a number was pressed
+        if sender.tag == 0 || sender.tag == 1 || sender.tag == 2 || sender.tag == 3 || sender.tag == 4 || sender.tag == 5 || sender.tag == 6 || sender.tag == 7 || sender.tag == 8 || sender.tag == 9{
+            // put the number into the string and push it to the display
+            theNumber += String(sender.tag)
+            mainDisplay.text = theNumber
+        }
+        // check if clear screen was pressed
+        else if sender.tag == 16{
+            // wipe the number and update it
+            theNumber = ""
+            mainDisplay.text = theNumber
+        }
+        // check if the del key was pressed
+        else if sender.tag == 10{
+            // get the index of the last character of the string
+            let index = theNumber.startIndex.advancedBy(theNumber.characters.count - 1)
+            // remove the last digit from the calculator
+            theNumber = theNumber.substringToIndex(index)
+            mainDisplay.text = theNumber
+        }
+    }
+    
+    // linking the label at the top
+    @IBOutlet weak var mainDisplay: UILabel!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
